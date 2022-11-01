cc64b30510845e18a544022533e5aa5ab4fbbbfb
Dario Balmaceda <dario26081993@uotlook.com.ar>
2022-11-01 19:23:52 -0300

    counter finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  
+  const handleRestar = () =>{
+    setCount(count-1);
+  } 
+
+  const handleReset = () =>{
+    setCount(0)
+  }
+
+  const handleSumar = () =>{
+    setCount(count + 1)
+  }
+
+  let color = "#fff";
+  if(count >= 1) {color = "green"}
+  else if( count < 0){color = "red"}
+  else{color = "white"}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter_sec'>
+      <div className='countainer counter'>
+        <h1>Counter APP</h1>
+        <p className='count' style={{color: color}}>{count}</p>
+        <dvi className="buttons">
+          <button className='res' onClick={handleRestar}> - Restar</button>
+          <button className='reset' onClick={handleReset}> Resetear </button>
+          <button className='sum' onClick={handleSumar}> + Sumar</button>
+        </dvi>
+      </div>
+
     </div>
   );
 }
